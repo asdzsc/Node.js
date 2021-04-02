fab65812fd86ee299f83d3df9ddfa9e0fc0d95c5
unknown <710972821@qq.com>
2021-04-02 11:35:27 +0800

    adminlte

--- AdminLTE/frontend/webpack.config.js
+++ AdminLTE/frontend/webpack.config.js
@@ -2,6 +2,8 @@ const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const CopyPlugin = require("copy-webpack-plugin");
 
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+
 module.exports = {
     // 配置环境
     mode: "development",
@@ -9,19 +11,23 @@ module.exports = {
     devtool: "source-map",
     // 文件入口
     entry: {
-        app: "./src/app.js",
+        "js/app": "./src/app.js",
     },
     // 文件的出口
     output: {
         path: path.resolve(__dirname, "./dist"),
-        filename: "app.js",
+        filename: "[name]-[hash:6].js",
     },
+    // module: {
+    //     rules: [{
+    //         test: /\.art$/,
+    //         loader: "art-template-loader",
+    //     }, ],
+    // },
     module: {
         rules: [{
             test: /\.art$/,
-            use: {
-                loader: "art-template-loader",
-            },
+            loader: "art-template-loader",
         }, ],
     },
     // 配置文件
@@ -40,10 +46,17 @@ module.exports = {
         // copy-webpack-plugin 的作用就是拷贝文件，或者文件夹 防止打包后找不到文件
         new CopyPlugin({
             patterns: [{
-                from: "./public/*.ico",
-                to: path.resolve(__dirname, "./dist"),
-            }, ],
+                    from: "./public/*.ico",
+                    to: path.resolve(__dirname, "./dist/favicon.ico"),
+                },
+                {
+                    from: "./public/libs",
+                    to: path.resolve(__dirname, "./dist/libs/"),
+                },
+            ],
         }),
+        //删除之前打包的文件
+        new CleanWebpackPlugin(),
     ],
     // 配置server
     devServer: {
--- lagou-admin/frontend/src/app.js
+++
@@ -1,5 +0,0 @@
-import indexTpl from "./views/home.art";
-
-const html = indexTpl({});
-console.log(html);
-$("#root").html(html);
